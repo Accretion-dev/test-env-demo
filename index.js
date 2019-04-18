b8d0e42bd3ae88026bbe60ffdaa5e4e0a100af8f
Fmajor <wujinnnnn@gmail.com>
2019-04-19 04:55:00 +0800

    and ava demo

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-import ACInput from './components/ac-input.vue'
-const plugin = {
-  install (Vue, options) {
-    Vue.component('ac-input', ACInput)
-  }
-}
-
-export default plugin
+export default {}
---
+++ test/ava.js
@@ -0,0 +1,6 @@
+import test from 'ava'
+import path from 'path'
+
+test('test', t => {
+	t.pass()
+})
